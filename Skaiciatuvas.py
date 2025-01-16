a9435ed059e4d6869cbb985f699dce0c17b8d9bb
TimofejDevks <timof.d02@gmail.com>
2025-01-16 10:36:39 +0200

    Update Skaiciatuvas.py
    skaicius

--- Skaiciatuvas.py
+++ Skaiciatuvas.py
@@ -1,10 +1,25 @@
-skaicius
+def skaičiatuvas():
+    print("Pasirinkite veiksmą:")
+    print("1. Sudėtis")
+    print("2. Atimtis")
+    print("3. Daugyba")
+    print("4. Dalyba")
 
+    pasirinkimas = input("Įveskite pasirinkto veiksmo numerį (1/2/3/4): ")
 
+    if pasirinkimas in ['1', '2', '3', '4']:
+        num1 = float(input("Įveskite pirmą skaičių: "))
+        num2 = float(input("Įveskite antrą skaičių: "))
 
+        if pasirinkimas == '1':
+            print(f"Rezultatas: {num1} + {num2} = {num1 + num2}")
+        elif pasirinkimas == '2':
+            print(f"Rezultatas: {num1} - {num2} = {num1 - num2}")
+        elif pasirinkimas == '3':
+            print(f"Rezultatas: {num1} * {num2} = {num1 * num2}")
+        elif pasirinkimas == '4':
+                print(f"Rezultatas: {num1} / {num2} = {num1 / num2}")
+    else:
+        print("Neteisingas pasirinkimas. Bandykite dar kartą.")
 
-
-
-
-
-asas
+skaičiatuvas()
