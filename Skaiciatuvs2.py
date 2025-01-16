02aa0165de76193faefcfe6c3803b1a028bca104
Michail <93334230+Mischail07@users.noreply.github.com>
2025-01-16 10:53:04 +0200

    Update Skaiciatuvs2.py
    if pasirinkimas -- skaiciatuvas

--- Skaiciatuvs2.py
+++ Skaiciatuvs2.py
@@ -11,6 +11,19 @@ if pasirinkimas in ['1', '2', '3', '4']:
         num1 = float(input("Įveskite pirmą skaičių: "))
         num2 = float(input("Įveskite antrą skaičių: "))
 
+if pasirinkimas == '1':
+            print(f"Rezultatas: {num1} + {num2} = {num1 + num2}")
+        elif pasirinkimas == '2':
+            print(f"Rezultatas: {num1} - {num2} = {num1 - num2}")
+        elif pasirinkimas == '3':
+            print(f"Rezultatas: {num1} * {num2} = {num1 * num2}")
+        elif pasirinkimas == '4':
+                print(f"Rezultatas: {num1} / {num2} = {num1 / num2}")
+    else:
+        print("Neteisingas pasirinkimas. Bandykite dar kartą.")
+
+skaičiatuvas()
+
 
 
 
