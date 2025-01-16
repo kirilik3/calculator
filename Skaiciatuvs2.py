caaa877bff93929029aed14a05192797501169f6
isaya <blynovkirill@gmail.com>
2025-01-16 11:26:05 +0200

    Update Skaiciatuvs2.py

--- Skaiciatuvs2.py
+++ Skaiciatuvs2.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-def skaičiatuvaslksjhdgidshgkhdsjghdsfghdjkgh nigga():
-=======
+
 def skaičiatuvs():
->>>>>>> a0f67c722491857d35481eb587f283e61a289016
     print("Pasirinkite veiksmą:")
     print("1. Sudėtis")
     print("2. Atimtis")
