bf665365bd37627a7f5db4bb0e80664061dd870a
isaya <starunicorn623@gmail.com>
2025-01-16 10:58:51 +0200

    Update Skaiciatuvs2.py

--- Skaiciatuvs2.py
+++ Skaiciatuvs2.py
@@ -7,11 +7,11 @@ def skaičiatuvas():
 
     pasirinkimas = input("Įveskite pasirinkto veiksmo numerį (1/2/3/4): ")
 
-if pasirinkimas in ['1', '2', '3', '4']:
+    if pasirinkimas in ['1', '2', '3', '4']:
         num1 = float(input("Įveskite pirmą skaičių: "))
         num2 = float(input("Įveskite antrą skaičių: "))
 
-if pasirinkimas == '1':
+        if pasirinkimas == '1':
             print(f"Rezultatas: {num1} + {num2} = {num1 + num2}")
         elif pasirinkimas == '2':
             print(f"Rezultatas: {num1} - {num2} = {num1 - num2}")
@@ -23,7 +23,6 @@ if pasirinkimas == '1':
         print("Neteisingas pasirinkimas. Bandykite dar kartą.")
 
 skaičiatuvas()
-
 
 def skaiciuotuvas(pasirinkimas, num1, num2):
     if pasirinkimas in ['1', '2', '3', '4']:
